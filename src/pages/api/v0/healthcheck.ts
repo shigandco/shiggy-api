3a8f44f1b387879dfc73b35d77568854b5862a31
Luna <imlvnaa@gmail.com>
2023-09-14 13:08:43 -0400

    [skip ci] return early

--- src/pages/api/v0/healthcheck.ts
+++ src/pages/api/v0/healthcheck.ts
@@ -5,12 +5,15 @@ let gotShiggies = false;
 
 export const GET: APIRoute = () => {
   if (!gotShiggies) {
-    getShiggies(
-      import.meta.env.MAX_SHIGGIES
-        ? Number.parseInt(import.meta.env.MAX_SHIGGIES)
-        : 100,
-    );
     gotShiggies = true;
+    setTimeout(() => {
+      getShiggies(
+        import.meta.env.MAX_SHIGGIES
+          ? Number.parseInt(import.meta.env.MAX_SHIGGIES)
+          : 100,
+      );
+    }, 1000);
+    return new Response("ok", { status: 200 });
   }
   return new Response("ok", { status: 200 });
 };
