ea15bb2a51459b1abde352f15750efedc8f1a6ee
voidfill <71205200+voidfill@users.noreply.github.com>
2023-09-14 14:41:54 +0200

    basic browser, fix getter for jpgs

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,15 @@
 import { AstroUserConfig, defineConfig } from "astro/config";
 import nodejs from "@astrojs/node";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 const config: AstroUserConfig = {
   output: "server",
+  integrations: [solid(), tailwind()],
   adapter: nodejs({
     mode: "standalone",
   }),
 };
+
+// https://astro.build/config
 export default defineConfig(config);
