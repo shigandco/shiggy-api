9a8039ead90d82d7ab95178292e7930569320892
Luna <imlvnaa@gmail.com>
2023-09-15 14:20:56 -0400

    Revert "Redirect instead of send"
    This reverts commit 7105772065be42485051e0722c530ffbb12a0c91.

--- src/pages/api/v3/random.ts
+++ src/pages/api/v3/random.ts
@@ -1,9 +1,15 @@
 import { APIRoute } from "astro";
 
+import { GET as shiggyGetter } from "./shiggies/[id]/index";
+
 import getShiggies from "../../../utils/getShiggies";
 
-export const GET: APIRoute = async ({ redirect }) => {
-  const chosenShiggy = Math.floor(Math.random() * (await getShiggies()).length);
+export const GET: APIRoute = async (APIContext) => {
+  const allShiggies = await getShiggies();
 
-  return redirect(`/api/v3/shiggies/${chosenShiggy}`);
+  const shig = allShiggies[Math.floor(Math.random() * allShiggies.length)];
+
+  APIContext.params = { id: shig };
+
+  return shiggyGetter(APIContext);
 };
