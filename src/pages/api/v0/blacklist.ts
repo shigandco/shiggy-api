c0205f5bde0685f1c9c7b9861163f7f5e0c2749b
Luna <imlvnaa@gmail.com>
2023-09-15 11:55:01 -0400

    Make the api route a post request

--- src/pages/api/v0/blacklist.ts
+++ src/pages/api/v0/blacklist.ts
@@ -2,7 +2,7 @@ import { APIRoute } from "astro";
 import { SHARED_KEY } from "../../../utils/filters";
 import emitter from "../../../events";
 
-export const GET: APIRoute = ({ request }) => {
+export const POST: APIRoute = ({ request }) => {
   if (!SHARED_KEY(request)) return new Response(null, { status: 401 });
 
   emitter.emit("UPDATE_SHIGGIES");
