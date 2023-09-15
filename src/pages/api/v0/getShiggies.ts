92476ecb7b992a1589553f8f2bf3cbeb68750148
Luna <imlvnaa@gmail.com>
2023-09-15 13:34:12 -0400

    Add authentication to getShiggies

--- src/pages/api/v0/getShiggies.ts
+++ src/pages/api/v0/getShiggies.ts
@@ -1,5 +1,7 @@
 import { APIRoute } from "astro";
-export const GET: APIRoute = ({ locals }) => {
+import { SHARED_KEY } from "../../../utils/filters";
+export const GET: APIRoute = ({ request, locals }) => {
+  if (!SHARED_KEY(request)) return new Response(null, { status: 401 });
   locals.getShiggies();
   return new Response(null, { status: 200 });
 };
