72db74758430dc4437901f5b7a5f9816d2e55cfd
katlyn <katlyn@katlyn.dev>
2023-10-01 13:52:05 -0800

    feat: add util function to load sizes file

--- src/utils/getShiggies.ts
+++ src/utils/getShiggies.ts
@@ -1,6 +1,12 @@
 import { join } from "path";
 import { SHIGGY_DIR } from "../constants";
 
-export default async function getShiggies() {
-  return (await Bun.file(join(SHIGGY_DIR, "shiggies.json")).json()) as string[];
+export default async function getShiggies(): Promise<string[]> {
+  return await Bun.file(join(SHIGGY_DIR, "shiggies.json")).json();
+}
+
+type ShiggySizeMap = { [key: string]: { width: number; height: number } };
+
+export async function getSizes(): Promise<ShiggySizeMap> {
+  return await Bun.file(join(SHIGGY_DIR, "sizes.json")).json();
 }
