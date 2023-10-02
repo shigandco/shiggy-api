760cf16b22a31180b4866c10aa25ba885296c4ad
Luna <imlvnaa@gmail.com>
2023-10-02 08:25:00 -0400

    Add back cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,9 @@
 import { handler } from "../../dist/server/entry.mjs";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use(express.static("dist/client"));
 
